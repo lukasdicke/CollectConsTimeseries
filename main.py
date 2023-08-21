f06040e3ae1a74eb9699e638457ff103a8f8f266
Lukas Dicke <lukas.dicke@web.de>
2023-08-21 13:29:18 +0200

    final

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ def GetContractSearchString(contractDict):
 
     ret=""
     for contract in contractDict:
-        myDictContracts.append("EXTERNAL_ID eq '" + contract + "' or ")
         ret = ret + "EXTERNAL_ID eq '" + contract + "' or "
 
-    ret = ret.removesuffix('or ')
+    ret = ret.removesuffix(' or ')
 
     return ret
+
+def FilenameConsReport():
+    return (datetime.today()).strftime("%Y%m%d_%H%M%S") +"_" + "CONSUMPTION_REPORT" + ".json"
+
+def PathConsReport():
+    return "\\\\energycorp.com\\common\\divsede\\Operations\\Personal_OPS\\Lukas\\"
 
 import pandas as pd
 from datetime import datetime
@@ -16,7 +21,7 @@ from dateutil.relativedelta import relativedelta
 from pytz import timezone
 from DeltaXE import Deltaxe_client
 
-begin = datetime.today() + timedelta(days=1)
+begin = datetime.today() + timedelta(days=0)
 date_from = pd.to_datetime(begin).normalize()
 
 #time difference between UTC and Europe/Berlin
@@ -28,78 +33,91 @@ offset = relativedelta(here, there)
 time_difference = offset.hours
 
 #initializations
-final_list = []
-timeseriesVolumes = []
+
 
 deltaXE = Deltaxe_client()
 
 myDictContracts=[]
+myDictGrids=[]
 myDictContracts.append('IMBAs50HuI')
 myDictContracts.append('IMBAsAMPuI')
 myDictContracts.append('IMBAsTENuI')
 myDictContracts.append('IMBAsTRNuI')
 
+myDictGrids.append('DE_AMPRION_N')
+myDictGrids.append('DE_TENNET_N')
+myDictGrids.append('DE_50HERTZ_N')
+myDictGrids.append('DE_TRANSNETBW_N')
 
-urlContractSearch = "Contract?$" \
-                    +"filter=DATE_FROM lt " + (date_from.replace(tzinfo=None) + timedelta(days=0)).isoformat() +\
-                    "Z and DATE_TO ge " + (date_from.replace(tzinfo=None) + timedelta(days=0)).isoformat() +\
-                    "Z and " + GetContractSearchString(myDictContracts) +\
-                    " and STATUS ne 'DELETED'"
+final_list = []
 
-responseContractSearch = deltaXE.get(urlContractSearch).json()
+for grid in myDictGrids:
 
-#cnt=len(responseContractSearch['value'])
+    timeseriesVolumes = []
 
-for contracts in responseContractSearch['value']:
+    urlContractSearch = "Contract?$" \
+                        +"filter=DATE_FROM lt " + (date_from.replace(tzinfo=None) + timedelta(days=0)).isoformat() +\
+                        "Z and DATE_TO ge " + (date_from.replace(tzinfo=None) + timedelta(days=0)).isoformat() +\
+                        "Z and (" + GetContractSearchString(myDictContracts) + ") and OUT_AREA eq '" + grid + "'" +\
+                        " and STATUS ne 'DELETED'"
 
-    contractID = str(contracts['CONTRACT_ID'])
-    urlContratLineSearch = "ContractLine?$filter=CONTRACT_ID eq " + contractID
-    responseContractLineSearch = deltaXE.get(urlContratLineSearch).json()
+    responseContractSearch = deltaXE.get(urlContractSearch).json()
 
-    for contractLines in responseContractLineSearch['value']:
+    for contracts in responseContractSearch['value']:
 
-        contractLineID = str(contractLines['CONTRACT_LINE_ID'])
-        urlContractPositionSearch = "ContractPosition?$filter=CONTRACT_LINE_ID eq " + contractLineID
+        contractID = str(contracts['CONTRACT_ID'])
+        urlContratLineSearch = "ContractLine?$filter=CONTRACT_ID eq " + contractID
+        responseContractLineSearch = deltaXE.get(urlContratLineSearch).json()
 
-        responseContractPositionSearch = deltaXE.get(urlContractPositionSearch).json()
+        for contractLines in responseContractLineSearch['value']:
 
-        for contractPosition in responseContractPositionSearch['value']:
+            contractLineID = str(contractLines['CONTRACT_LINE_ID'])
+            urlContractPositionSearch = "ContractPosition?$filter=CONTRACT_LINE_ID eq " + contractLineID
 
-            if contractPosition['POSITION_TYPE'] == 'QUANTITY':
+            responseContractPositionSearch = deltaXE.get(urlContractPositionSearch).json()
 
-                dateTo = (date_from.replace(tzinfo=None) + timedelta(0) + timedelta(hours=time_difference)).isoformat()
-                dateFrom = (date_from.replace(tzinfo=None) + timedelta(-1) + timedelta(hours=time_difference)).isoformat()
+            for contractPosition in responseContractPositionSearch['value']:
 
-                positionID = str(contractPosition['POSITION_ID'])
+                if contractPosition['POSITION_TYPE'] == 'QUANTITY':
 
-                #Known issue/feature: DateFrom & DateTo are switched
-                urlContractLines = "TimeSeriesQuarterHour?$filter=POSITION_ID eq " + positionID + " and DATE_FROM lt " + dateTo + "Z and DATE_TO gt " + dateFrom + "Z"
-                responseContractLines = deltaXE.get(urlContractLines).json()
+                    dateTo = (date_from.replace(tzinfo=None) + timedelta(0) + timedelta(hours=time_difference)).isoformat()
+                    dateFrom = (date_from.replace(tzinfo=None) + timedelta(-1) + timedelta(hours=time_difference)).isoformat()
 
-                timeseriesVolumes = []
-                for contractLine in responseContractLines['value']:
-                    if contractLine['VALUE'] == None:
-                        pass
-                    else:
-                        # All values will be positive
-                        timeseriesVolumes.append(contractLine["VALUE"])
-                if timeseriesVolumes:
+                    positionID = str(contractPosition['POSITION_ID'])
 
+                    #Known issue/feature: DateFrom & DateTo are switched
+                    urlContractLines = "TimeSeriesQuarterHour?$filter=POSITION_ID eq " + positionID + " and DATE_FROM lt " + dateTo + "Z and DATE_TO gt " + dateFrom + "Z"
+                    responseContractLines = deltaXE.get(urlContractLines).json()
 
+                    timeseriesVolumes = []
+                    for contractLine in responseContractLines['value']:
+                        if contractLine['VALUE'] == None:
+                            pass
+                        else:
+                            # All values will be positive
+                            timeseriesVolumes.append(contractLine["VALUE"])
+                    if timeseriesVolumes:
 
-                    myDict = {"COUNTERPARTY": contracts["COUNTERPARTY"],
-                              "DATE_FROM": (date_from + timedelta(-1) + timedelta(hours=0)).isoformat(),
-                              "DATE_TO": (date_from + timedelta(days=0) + timedelta(hours=0)).isoformat(),
-                              "NAME": contracts["NAME"],
-                              "EXTERNAL_ID": contracts["EXTERNAL_ID"],
-                              "AGG_VOLUME": (sum(timeseriesVolumes) / 4),
-                              "VOLUME_TS": ";".join(str(i) for i in timeseriesVolumes)}
+                        myDict = {"COUNTERPARTY": contracts["COUNTERPARTY"],
+                                  # "DATE_FROM": (date_from + timedelta(-1) + timedelta(hours=0)).isoformat(),
+                                  "DELIVERY_DATE": (datetime.today()).strftime("%d.%m.%Y"),
+                                  "NAME": contracts["NAME"],
+                                  "EXTERNAL_CONTRACT_ID": contracts["EXTERNAL_ID"],
+                                  "CONTROL_AREA": contracts["OUT_AREA"],
+                                  "BALANCE": contracts["BALANCE"],
+                                  "AGG_VOLUME": (sum(timeseriesVolumes) / 4),
+                                  "VOLUME_TS": ";".join(str(i) for i in timeseriesVolumes)}
 
-                    final_list.append(myDict)
+                        final_list.append(myDict)
 
 df = pd.DataFrame.from_dict(final_list)
-#df.to_csv("\\\\energycorp.com\\common\\divsede\\Operations\\Personal_OPS\\Lukas\\"+date_from.strftime("%d.%m.%Y")+"_Amprion_prod.csv", sep=";")
-df.to_json("\\\\energycorp.com\\common\\divsede\\Operations\\Personal_OPS\\Lukas\\"+date_from.strftime("%d.%m.%Y")+"_Amprion_prod.json", orient='split')
+
+path = PathConsReport() + FilenameConsReport
+
+#df.to_csv(path, sep=";")
+df.to_json(path, orient='records', indent=  True)
 df
+
+
 
 print("Done")
